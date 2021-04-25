12e87945cf3c6c033ec907c82fb117c9aea7c05c
Marcos Rezende <rezehnde@gmail.com>
2021-04-25 09:48:57 +0100

    Minor changes on deptrac

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use App\Service\ProductFormatter;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
